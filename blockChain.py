b8edbcfd96c47389962458d4b36688a4bb3039b9
CosmicCoder96 <abhinav.khare31@gmail.com>
2018-02-09 22:54:38 +0530

    Add POW

--- blockChain.py
+++ blockChain.py
@@ -1,7 +1,7 @@
 import hashlib
 import json
 from time import time
-
+from uuid import uuid
 
 class Blockchain(object):
     def __init__(self):
@@ -47,6 +47,37 @@ class Blockchain(object):
         })
 
         return self.last_block['index'] + 1
+
+    def proof_of_work(self, last_proof):
+        """
+        Simple Proof of Work Algorithm:
+         - Find a number p' such that hash(pp') contains leading 4 zeroes, where p is the previous p'
+         - p is the previous proof, and p' is the new proof
+
+        :param last_proof: <int>
+        :return: <int>
+        """
+
+        proof = 0
+        while self.valid_proof(last_proof, proof) is False:
+            proof += 1
+
+        return proof
+
+    @staticmethod
+    def valid_proof(last_proof, proof):
+        """
+        Validates the Proof: Does hash(last_proof, proof) contain 4 leading zeroes?
+
+        :param last_proof: <int> Previous Proof
+        :param proof: <int> Current Proof
+        :return: <bool> True if correct, False if not.
+        """
+
+        guess = f'{last_proof}{proof}'.encode()
+        guess_hash = hashlib.sha256(guess).hexdigest()
+        return guess_hash[:4] == "0000"
+
 
     @staticmethod
     def hash(block):
