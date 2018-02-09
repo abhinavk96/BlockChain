f24bd83d616c07d86f6cab5158806839569d1db3
CosmicCoder96 <abhinav.khare31@gmail.com>
2018-02-09 22:45:30 +0530

    Get the structure of block chain ready

--- blockChain.py
+++ blockChain.py
@@ -1,11 +1,35 @@
+import hashlib
+import json
+from time import time
+
+
 class Blockchain(object):
     def __init__(self):
         self.chain = []
         self.current_transactions = []
 
-    def new_block(self):
-        # Creates a new block and adds it to the chain
-        pass
+        # Create the genesis block
+        self.new_block(previous_hash=1, proof=100)
+
+    def new_block(self, proof, previous_hash=None):
+        """
+
+        :param proof: <int> The proof given by the Proof Of Work Algorithm
+        :param previous_hash: (Optional) <str> Hash of the previous block
+        :return: <dict> New Block
+        """
+        block = {
+            'index': len(self.chain) + 1,
+            'timestamp': time(),
+            'transactions': self.current_transactions,
+            'proof': proof,
+            'previous_hash': previous_hash or self.hash(self.chain[-1])
+        }
+
+        # Reset the current list of transactions
+        self.current_transactions = [],
+        self.chain.append(block)
+        return block
 
     def new_transaction(self, sender, recipient, amount):
         """
@@ -26,10 +50,15 @@ class Blockchain(object):
 
     @staticmethod
     def hash(block):
-        # Hashes a block
-        pass
+        """
+        Creates a SHA-256 hash of a Block
+        :param block: <dict> Block
+        :return: <str>
+        """
+        block_string = json.dumps(block, sort_keys=True).encode()
+        return hashlib.sha256(block_string).hexdigest()
 
     @property
     def last_block(self):
         # Returns the last block in the chain
-        pass
+        return self.chain[-1]
